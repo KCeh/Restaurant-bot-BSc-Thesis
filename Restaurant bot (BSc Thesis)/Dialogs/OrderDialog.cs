9c4f56373a578e29dff20914bfe94e97ec90a0f0
Karlo Čeh <karlo.ceh@fer.hr>
2018-03-21 19:59:02 +0100

    Added enums for orders (DB not in plan for now)

---
+++ Restaurant bot (BSc Thesis)/Dialogs/OrderDialog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant_bot__BSc_Thesis_.Dialogs
+{
+    public class OrderDialog
+    {
+    }
+}
---
+++ Restaurant bot (BSc Thesis)/Order.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant_bot__BSc_Thesis_
+{
+    // 0 value in enums is reserved for unknown values. 
+    public enum Food
+    {
+        Burrito=1,
+        Cheeseburger,
+        ChickenSandwich,
+        FriedChicken,
+        Hamburger,
+        HotDog,
+        Kebab,
+        Oatmeal,
+        Taco,
+        VeggieBurger
+    };
+
+    public enum SaladsAndSncks
+    {
+        CaesarSalad=1,
+        ChickenSalad,
+        MashedPotatoes,
+        OnionRings,
+        PotatoFries,
+        Veggies
+    };
+
+    public enum Drinks
+    {
+        AppleJuice,
+        Cappuccino,
+        ChocolateMilkshake,
+        CokeSoda,
+        Espresso,
+        IceTea,
+        Latte,
+        Lemonade,
+        Macchiato,
+        OrangeJuice
+    };
+
+    public enum Desserts
+    {
+        ApplePie,
+        Cheesecake,
+        ChocolateCake,
+        ChocolateCookies,
+        Cupcake,
+        IceCream,
+        JellyBean,
+        LemonBars,
+        Pudding,
+        Tiramisu
+    };
+
+    public class Order
+    {
+    }
+}
