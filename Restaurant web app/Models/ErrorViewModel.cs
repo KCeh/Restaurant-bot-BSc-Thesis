8cf2e6496040ce6c2859632081a37ae5a4a5a3d8
Karlo Čeh <karlo.ceh@fer.hr>
2018-04-21 13:27:00 +0200

    added web app prototype

---
+++ Restaurant web app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Restaurant_web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
